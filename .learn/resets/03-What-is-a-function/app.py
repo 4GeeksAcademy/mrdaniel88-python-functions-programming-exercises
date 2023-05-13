c5d2bc10a4f375c801d50489cb28a63de92bb151
Daniel Martinez <d.martinez74@gmail.com>
2023-05-13 03:00:27 +0000

    done until 9

--- .learn/resets/03-What-is-a-function/app.py
+++ .learn/resets/03-What-is-a-function/app.py
@@ -1,5 +1,5 @@
 def sum(number1,number2):
     return number1 + number2
 
-total = sum(3445324,53454423)
+total = sum(2,3)
 print(total)
--- exercises/08-Function-that-returns/app.py
+++ exercises/08-Function-that-returns/app.py
@@ -5,3 +5,7 @@ def euro_to_yen(euro_value):
 	return euro_value * 124.15
 
 ####### ↓ YOUR CODE BELOW ↓ #######
+result_dollar_to_euro = dollar_to_euro(137)
+result_euro_to_yen = euro_to_yen(result_dollar_to_euro)
+
+print(result_euro_to_yen)
